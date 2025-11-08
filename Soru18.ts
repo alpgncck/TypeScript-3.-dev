c52138b6f6dddfb589a3f982088002925f817399
baturalp <squenly0660@hotmail.com>
2025-11-08 11:40:50 +0300

    Soru18.ts

---
+++ Soru18.ts
@@ -0,0 +1,13 @@
+type LastParameter<T> = T extends (...args: [...any[], infer L]) => any ? L : never;
+
+// ---- KOD BLOKU  ----
+const fn1 = (a: string, b: number, c: boolean) => {};
+const fn2 = (a: Date) => {};
+
+let p1: LastParameter<typeof fn1> = true;
+let p2: LastParameter<typeof fn2> = new Date();
+
+// @ts-expect-error - 'fn1'in son parametresi 'boolean', 'string' değil
+let errorP: LastParameter<typeof fn1> = "string";
+
+console.log("Soru 18 Başarılı!", p1);

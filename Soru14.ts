15f59f051ad00b2f43597bce836d68dda8c7d9aa
baturalp <squenly0660@hotmail.com>
2025-11-08 11:32:04 +0300

    Soru14.ts

---
+++ Soru14.ts
@@ -0,0 +1,20 @@
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+// ---- KOD BLOKU  ----
+type Config = { api: { url: string; }, features: string[] };
+const config: DeepReadonly<Config> = {
+  api: { url: "http://..." },
+  features: ["A", "B"]
+};
+
+// @ts-expect-error - Bu atamanın HATA vermesi beklenir
+config.api.url = "yeni-url";
+
+// @ts-expect-error - Bu atamanın (diziye ekleme) HATA vermesi beklenir
+config.features.push("C");
+
+console.log("Soru 14 Başarılı!", config.api.url);
+// BEKLENEN "EKRAN ÇIKTISI":
+// (Derleyicide hata olmaması ve konsolda "Soru 14 Başarılı!" yazması)
